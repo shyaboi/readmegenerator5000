a2347d5567db1d4b89c8f3c098b943db53176106
Ian <ian.shane.sears@gmail.com>
2020-03-01 14:10:13 -0700

    axios and fs added, getting github repos creations to new txt file.

--- index.js
+++ index.js
@@ -1,22 +1,28 @@
-/**
- * Nested Inquirer call
- */
-
-'use strict';
-var inquirer = require('inquirer');
+const fs = require("fs");
+const axios = require("axios");
+const inquirer = require("inquirer");
 
 inquirer
   .prompt({
-    type: 'list',
-    name: 'chocolate',
-    message: "What's your favorite chocolate?",
-    choices: ['Mars', 'Oh Henry', 'Hershey']
+    message: "Enter your GitHub username:",
+    name: "username"
   })
-  .then(() => {
-    inquirer.prompt({
-      type: 'list',
-      name: 'beverage',
-      message: 'And your favorite beverage?',
-      choices: ['Pepsi', 'Coke', '7up', 'Mountain Dew', 'Red Bull']
+  .then(function({ username }) {
+    const queryUrl = `https://api.github.com/users/${username}/repos?per_page=100`;
+
+    axios.get(queryUrl).then(function(res) {
+      const repoNames = res.data.map(function(repo) {
+        return repo.name;
+      });
+
+      const repoNamesStr = repoNames.join("\n");
+
+      fs.writeFile("repos.txt", repoNamesStr, function(err) {
+        if (err) {
+          throw err;
+        }
+
+        console.log(`Saved ${repoNames.length} repos`);
+      });
     });
   });
